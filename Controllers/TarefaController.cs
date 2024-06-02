bc869600e9466740ef852e7040bddb902d14b218
João Aquino <dhunyaquino@gmail.com>
2024-06-02 20:36:20 -0300

    Dessafio Concluidogit add Controllers/TarefaController.csgit add Controllers/TarefaController.csgit add Controllers/TarefaController.cs

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,25 +18,30 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
-            // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            var tarefa = _context.Tarefas.Find(id);
+            if (tarefa ==null)
+                return NotFound();
+
+            return Ok(tarefa);
+            //Implementado!!!
         }
 
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            var todasAsTarefas = _context.Tarefas.ToList();
+            
+            return Ok(todasAsTarefas);
+            //Implementado!!!
         }
 
         [HttpGet("ObterPorTitulo")]
         public IActionResult ObterPorTitulo(string titulo)
         {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
-            return Ok();
+            var tarefasComTitulo =_context.Tarefas.Where(x => x.Titulo.Contains(titulo)).ToList();
+         
+            return Ok(tarefasComTitulo);
+            //Implementado!!!
         }
 
         [HttpGet("ObterPorData")]
@@ -49,10 +54,11 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("ObterPorStatus")]
         public IActionResult ObterPorStatus(EnumStatusTarefa status)
         {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
-            var tarefa = _context.Tarefas.Where(x => x.Status == status);
-            return Ok(tarefa);
+            var tarefasComStatus = _context.Tarefas.Where(x => x.Status == status).ToList();
+
+            // Implentado!!!
+          
+            return Ok(tarefasComStatus);
         }
 
         [HttpPost]
@@ -60,8 +66,10 @@ namespace TrilhaApiDesafio.Controllers
         {
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
+                _context.Tarefas.Add(tarefa);
+                _context.SaveChanges();
 
-            // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+            // Implementado!!!
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
@@ -75,9 +83,14 @@ namespace TrilhaApiDesafio.Controllers
 
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
+            tarefaBanco.Titulo = tarefa.Titulo;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Status = tarefa.Status;
 
-            // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
-            // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
+            _context.SaveChanges();
+
+            // Implementado!!!
             return Ok();
         }
 
@@ -88,8 +101,10 @@ namespace TrilhaApiDesafio.Controllers
 
             if (tarefaBanco == null)
                 return NotFound();
+            _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
 
-            // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
+            //Implementado!!!
             return NoContent();
         }
     }
